484353b8f51521726faf754ca1becffb56e8516c
NorbertKocsis <norbert.kocsis@liferay.com>
2015-11-26 13:54:14 +0100

    LPS-60794 Add UserModelListener to reindex after modifying associations

---
+++ portal-impl/src/com/liferay/portal/model/UserModelListener.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model;
+
+import com.liferay.portal.ModelListenerException;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.security.permission.PermissionCacheUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+/**
+ *
+ * @author Norbert Kocsis
+ *
+ */
+public class UserModelListener extends BaseModelListener<User> {
+
+	@Override
+	public void onAfterAddAssociation(
+			Object userId, String associationClassName,
+			Object associationClassPK)
+		throws ModelListenerException {
+
+		try {
+			reindex((Long)userId);
+		}
+		catch (Exception e) {
+			throw new ModelListenerException(e);
+		}
+	}
+
+	@Override
+	public void onAfterRemoveAssociation(
+			Object userId, String associationClassName,
+			Object associationClassPK)
+		throws ModelListenerException {
+
+		try {
+			reindex((Long)userId);
+		}
+		catch (Exception e) {
+			throw new ModelListenerException(e);
+		}
+	}
+
+	protected void reindex(long userId) throws Exception {
+		Indexer<User> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
+			User.class);
+
+		User user = UserLocalServiceUtil.fetchUser(userId);
+
+		indexer.reindex(user);
+
+		PermissionCacheUtil.clearCache(userId);
+	}
+
+}
