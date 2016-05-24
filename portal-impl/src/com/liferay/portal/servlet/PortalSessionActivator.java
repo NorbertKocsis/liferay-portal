df6dd6180872846cf3e7e2795b228d0edcdf101a
NorbertKocsis <norbert.kocsis@liferay.com>
2016-05-24 17:55:09 +0200

    LPS-65236 Add PortalSessionActivator PortalSessionPassivator

---
+++ portal-impl/src/com/liferay/portal/servlet/PortalSessionActivator.java
@@ -0,0 +1,48 @@
+package com.liferay.portal.servlet;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.PortalSessionContext;
+import com.liferay.portal.kernel.util.BasePortalLifecycle;
+import com.liferay.portal.util.PropsValues;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+
+/**
+ * Created by Kocsis Norbert on 5/24/2016.
+ */
+public class PortalSessionActivator extends BasePortalLifecycle {
+	public PortalSessionActivator(HttpSessionEvent httpSessionEvent) {
+		_httpSessionEvent = httpSessionEvent;
+
+		registerPortalLifecycle(METHOD_INIT);
+	}
+
+	@Override
+	protected void doPortalDestroy() {
+	}
+
+	@Override
+	protected void doPortalInit() {
+		if (PropsValues.SESSION_DISABLED) {
+			return;
+		}
+
+		HttpSession session = _httpSessionEvent.getSession();
+
+		try {
+			PortalSessionContext.put(session.getId(), session);
+		}
+		catch (IllegalStateException ise) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(ise, ise);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PortalSessionCreator.class);
+
+	private final HttpSessionEvent _httpSessionEvent;
+}
